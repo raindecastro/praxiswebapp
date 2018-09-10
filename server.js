8e677dbea23bd135fa84e94f5e8ad15691f42b79
Rain <rainvincentdecastro@gmail.com>
2018-09-11 04:44:30 +0800

    last try for today

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const os = require('os');
-
+const path = require('path');
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('/dist'));
+
+  // Express serve up index.html file if it doesn't recognize route
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
+}
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) =>
