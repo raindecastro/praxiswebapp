77d443f759db863c0f04339efd36bc4170335a9b
Rain <rainvincentdecastro@gmail.com>
2018-09-11 04:55:45 +0800

    last try for today

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('/dist'));
+  app.use(express.static('dist'));
 
   // Express serve up index.html file if it doesn't recognize route
 
