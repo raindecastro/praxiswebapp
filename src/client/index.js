bcd466856647d6fa389ff628b7c7a199e77dd072
Rain <rainvincentdecastro@gmail.com>
2019-01-03 15:03:14 +0800

    buildpack for heroku

--- src/client/index.js
+++ src/client/index.js
@@ -10,10 +10,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
