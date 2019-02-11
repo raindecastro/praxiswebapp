145aaed0743d38138b13c67f1f8f533267c5abd5
Rain <rainvincentdecastro@gmail.com>
2019-02-11 13:18:40 +0800

    for live prod

--- src/client/common/Footer.js
+++ src/client/common/Footer.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import styles from './styles/_footer.scss';
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from 'react-scroll';
+import { HashLink } from 'react-router-hash-link';
 
-const Footer = () => (
+const Footer = props => (
   <div className={styles.footerContainer}>
     <div className={styles.leftLinks}>
-      <p className={styles.praxisSmallParagraph}>HOW TO PLAY</p>
-      <p className={styles.praxisSmallParagraph}>PRICING</p>
-      <p className={styles.praxisSmallParagraph}>NEWS</p>
-      <p className={styles.praxisSmallParagraph}>FAQ</p>
-      <p className={styles.praxisSmallParagraph}>ABOUT US</p>
-      <p className={styles.praxisSmallParagraph}>CONTACT US</p>
+      <p
+        onClick={() => {
+          window.location.href = '/about';
+        }}
+        className={styles.praxisSmallParagraph}
+      >
+        ABOUT US
+      </p>
+      <HashLink to="/#pricing">
+        {' '}
+        <p className={styles.praxisSmallParagraph}>PRICING</p>
+      </HashLink>
+
+      <p
+        onClick={() => {
+          window.location.href = '/news';
+        }}
+        className={styles.praxisSmallParagraph}
+      >
+        NEWS
+      </p>
+      <p
+        onClick={() => {
+          window.location.href = '/contact';
+        }}
+        className={styles.praxisSmallParagraph}
+      >
+        CONTACT US
+      </p>
     </div>
     <div className={styles.rightLinks}>
-      <p className={styles.praxisSmallParagraph}>FOLLOW US:</p>
+      {/* <p className={styles.praxisSmallParagraph}>FOLLOW US:</p> */}
     </div>
   </div>
 );
